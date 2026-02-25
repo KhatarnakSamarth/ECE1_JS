dda475e0793a29955e07e5133983b6825fee51f8
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-02-25 10:09:51 +0530

    Symbol Datatype

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+// Primitive DataTypes
 let x;
-console.log("X = ", x, "\t| TypeOf X: ", typeof(x));
+console.log("X = ", x, "\t| TypeOf X: ", typeof(x));    // Undefined
 
 x = 69;
-console.log("X = ", x, "\t| TypeOf X: ", typeof(x));
+console.log("X = ", x, "\t| TypeOf X: ", typeof(x));    // Number
 
 x = "Hello JS";
-console.log("X = ", x, "\t| TypeOf X: ", typeof(x));
+console.log("X = ", x, "\t| TypeOf X: ", typeof(x));    // String
 
 x = true;
-console.log("X = ", x, "\t| TypeOf X: ", typeof(x));
+console.log("X = ", x, "\t| TypeOf X: ", typeof(x));    // Boolean
 
 x = null;
-console.log("X = ", x, "\t| TypeOf X: ", typeof(x));
+console.log("X = ", x, "\t| TypeOf X: ", typeof(x));    // Null
 
+
+let sym1 = Symbol(4);
+let sym2 = Symbol(4);
+
+console.log(sym1, sym2)
+
+(sym1 == sym2) ? console.log("Same") : console.log("Not Equal Symbol")
