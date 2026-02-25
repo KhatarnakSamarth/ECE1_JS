1a6586d3f6e238fa48c5bac92db5c61dfa56026c
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-02-25 09:48:07 +0530

    JS Variables Datatype and TypeOf method

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+let x;
+console.log("X = ", x, "\t| TypeOf X: ", typeof(x));
+
+x = 69;
+console.log("X = ", x, "\t| TypeOf X: ", typeof(x));
+
+x = "Hello JS";
+console.log("X = ", x, "\t| TypeOf X: ", typeof(x));
+
+x = true;
+console.log("X = ", x, "\t| TypeOf X: ", typeof(x));
+
+x = null;
+console.log("X = ", x, "\t| TypeOf X: ", typeof(x));
+
