8d0739c00c37fda1a99e554336278108ea27246c
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-11 10:04:02 +0530

    FUNCTION in JS

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
 "use strict"
 
-let age = 20;
-
-
-switch (age) {
-    case 18:
-        window.alert("You just Got 18! \nYou can Apply For Learning DL Now.")
-        break;
-
-    case 50:
-        window.alert("You are 50! \nApply For New DL Now.")
-        break;
-
-    default:
-        if (age > 18) {
-            window.alert("Your age is : " + age + "\nYou are allowed to Drive a Car.");
-        }
-        else {
-            window.alert("Your age is : " + age + "\nYou are a Minor, Can't Drive");
-        }
-
-        break;
+function btnClicked() {
+    window.alert("YOU CLICKED THE BUTTON !!!");
+    console.log("Clicked Button!");
 }
