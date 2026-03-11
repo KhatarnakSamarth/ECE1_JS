5ec333d69881783399f82e8d33fbd8fdd44ee10f
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-11 09:47:53 +0530

    SWITCH CASE Statements in JS

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 "use strict"
 
 let age = 20;
-if (age > 18){
-    window.alert("Your age is : " + age + "\nYou are allowed to Drive a Car.");
-}
-else{
-    window.alert("Your age is : " + age + "\nYou are a Minor, Can't Drive");
+
+
+switch (age) {
+    case 18:
+        window.alert("You just Got 18! \nYou can Apply For Learning DL Now.")
+        break;
+
+    case 50:
+        window.alert("You are 50! \nApply For New DL Now.")
+        break;
+
+    default:
+        if (age > 18) {
+            window.alert("Your age is : " + age + "\nYou are allowed to Drive a Car.");
+        }
+        else {
+            window.alert("Your age is : " + age + "\nYou are a Minor, Can't Drive");
+        }
+
+        break;
 }
