d4adaea1d8cfe185f881c80abc9b45598135df6a
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-11 10:24:56 +0530

    OBJECT using Object Class

--- script.js
+++ script.js
@@ -3,9 +3,19 @@
 // OBJECT
 
 let person = {
-    name : "Rohan das",
-    age : 20,
-    gender : "Male"
+    name: "Rohan das",
+    age: 20,
+    gender: "Male"
 }
 
 console.log(person);
+
+
+
+let person2 = new Object();
+person2.name = "Shyam Singh";
+person2['age'] = 34;
+person2.gender = "Male";
+
+console.log(person2);
+
