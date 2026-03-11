4db1a9d19bcea67421719d0330cc30144f79b76d
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-11 09:28:09 +0530

    WHILE LOOP in JS

--- script.js
+++ script.js
@@ -3,10 +3,13 @@
 // FOR LOOP
 
 document.write("<h1>FOR LOOP</h1>")
-for (let count = 0; count < 6; count++) {
+for (var count = 0; count < 6; count++) {
     console.log(count);
     document.write("Current Count: " + count + "</br>");
 }
+
+
+document.write("</br>");
 
 
 // FOR IN LOOP
@@ -26,4 +29,15 @@ for(let x in person){
 }
 
 
+document.write("</br>");
 
+
+// WHILE LOOP
+
+document.write("<h1>WHILE LOOP</h1>")
+var count = 0;
+while (count < 6) {
+    console.log(count);
+    document.write("Current Count: " + count + "</br>");
+    count++;
+}
