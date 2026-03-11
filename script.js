ec5d8cc5c06cf1243734ac018b266dd42d1dd071
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-11 09:37:28 +0530

    IF ELSE statement

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 "use strict"
 
-
-
 let age = 20;
 if (age > 18){
     window.alert("Your age is : " + age + "\nYou are allowed to Drive a Car.");
+}
+else{
+    window.alert("Your age is : " + age + "\nYou are a Minor, Can't Drive");
 }
