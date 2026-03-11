66348f65dbb7f936b6716e0bc86cea19d4f5a22e
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-11 09:23:38 +0530

    FOR IN LOOP in JS

--- script.js
+++ script.js
@@ -2,8 +2,28 @@
 
 // FOR LOOP
 
-document.write("<h1>FOR LOOP</h1></br>")
+document.write("<h1>FOR LOOP</h1>")
 for (let count = 0; count < 6; count++) {
     console.log(count);
     document.write("Current Count: " + count + "</br>");
 }
+
+
+// FOR IN LOOP
+
+document.write("<h1>FOR IN LOOP</h1>")
+let person = {
+    fname : "Rohan",
+    lname : "Das",
+    age : 25
+}
+
+console.log("Person Details:");
+document.write("<h2>Person Details: </h2>");
+for(let x in person){
+    console.log( x + " : " + person[x]);
+    document.write( x + " : " + person[x] + "</br>");
+}
+
+
+
