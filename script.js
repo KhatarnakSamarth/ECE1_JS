b833cea791951e294c2b09b37f804557521a5adc
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-11 10:10:57 +0530

    ARROW FUNCTION in JS

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function btnClicked() {
     window.alert("YOU CLICKED THE BUTTON !!!");
     console.log("Clicked Button!");
 }
+
+
+// ARROW FUNCTION is an Anonymous Function
+
+var myFunc = () =>{
+    console.log("Hello");
+}
