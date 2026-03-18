b0cd47de65e5c5764d6ce3f93d6b306d13fa35d2
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-18 09:37:32 +0530

    querySelector in DOM

--- DOM.js
+++ DOM.js
@@ -1,21 +1,5 @@
-
-// Get Element By ID
-
-let container = document.getElementById('container');
-console.log(container);
-
-
-
-// Get Element By Class Name
-
-let box = document.getElementsByClassName('box');
-console.log(box);
-
-
-// Get Element By Tag Name
-
-let div = document.getElementsByTagName('div');
-console.log(div);
-
+// Query Selector
+let queryBox = document.querySelector('.box');
+console.log(queryBox);
 
 
