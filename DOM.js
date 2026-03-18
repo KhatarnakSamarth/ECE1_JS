65a09dcb5a5c538f52123b9af6430274181d100d
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-18 09:45:49 +0530

    setAttribute Method

--- DOM.js
+++ DOM.js
@@ -1,4 +1,4 @@
-// get Attribute
+// set Attribute
 
-let result = document.getElementById('container').getAttribute('data-attribute');
-console.log(result);
+document.getElementById('container').setAttribute('data-attribute-added', 'WeAddedAttribute');
+
