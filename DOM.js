82b71194da505ccecb0b0403f4e329ec97158cb2
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-18 10:00:26 +0530

    Adding CSS Styles using JS

--- DOM.js
+++ DOM.js
@@ -1,18 +1,6 @@
-// Create an Element
+// Changing Style
 
-let element = document.createElement('h1');
-let body = document.querySelector('body');
-
-body[0].append(element);
-
-
-// ============ 1. Before ============
-// <body>
-// ============ 2. Prepend ============
-
-// <childs>
-// </childs>
-
-// ============ 3. Append ============
-// </body>
-// ============ 4. After ============
+document.querySelectorAll('.box').forEach(element => {
+    element.style.backgroundColor = 'red';
+    element.style.border = '2px solid black';
+});
