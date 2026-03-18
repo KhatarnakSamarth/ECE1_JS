229c28362bb21cb814db7d5d34114d31e011f405
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-18 09:35:38 +0530

    3 Methods to select HTML Element

--- DOM.js
+++ DOM.js
@@ -1,4 +1,21 @@
+
 // Get Element By ID
+
+let container = document.getElementById('container');
+console.log(container);
+
+
+
+// Get Element By Class Name
+
+let box = document.getElementsByClassName('box');
+console.log(box);
+
+
+// Get Element By Tag Name
 
 let div = document.getElementsByTagName('div');
 console.log(div);
+
+
+
