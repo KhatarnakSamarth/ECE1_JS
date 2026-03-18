3ef15e700686145779ea5cc5a0d74d078b3c9d41
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-18 10:16:58 +0530

    Mouse Events in JS

--- DOM.js
+++ DOM.js
@@ -1,6 +1,22 @@
-// Changing Style
 
-document.querySelectorAll('.box').forEach(element => {
-    element.style.backgroundColor = 'red';
-    element.style.border = '2px solid black';
-});
+let btns = document.getElementsByClassName('btn');
+
+function onclick(){
+    console.log('Single Click');
+}
+function ondblclick(){
+    console.log('Double Click');
+}
+function onmousedown(){
+    console.log('Mouse Down');
+}
+function onmouseup(){
+    console.log('Mouse Up');
+}
+function onmouseover(){
+    console.log('Mouse Over');
+}
+function onmouseout(){
+    console.log('Mouse Out');
+}
+
