4655482461ba2441f94d70f9367a8872d67042d0
SAMARTH VERMA <232195837+KhatarnakSamarth@users.noreply.github.com>
2026-03-18 09:39:39 +0530

    All Methods to Select Elements from HTML DOM

--- DOM.js
+++ DOM.js
@@ -1,5 +1,29 @@
-// Query Selector All
-let queryBox = document.querySelectorAll('.box');
+
+// Get Element By ID
+
+let container = document.getElementById('container');
+console.log(container);
+
+
+// Get Element By Class Name
+
+let box = document.getElementsByClassName('box');
+console.log(box);
+
+
+// Get Element By Tag Name
+
+let div = document.getElementsByTagName('div');
+console.log(div);
+
+
+// Query Selector 
+let queryBox = document.querySelector('.box');
 console.log(queryBox);
+
+
+// Query Selector 
+let queryBoxAll = document.querySelectorAll('.box');
+console.log(queryBoxAll);
 
 
